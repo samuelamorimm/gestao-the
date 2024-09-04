feec31b772bc9e3b169cfe9b697936bfb2316e20
samuelamorimm <samuelhenriqueamorim@gmail.com>
2024-09-04 12:07:39 -0300

    correção de detalhes

--- addProduto.js
+++ addProduto.js
@@ -1,6 +1,5 @@
-var nomeProduto = document.getElementById("nomeNovoProduto").value
-var valorProduto = document.getElementById("valorNovoProduto")
 var descProduto = document.getElementById("descNovoProduto")
+
 
 var produtoBase = document.getElementById("produtoBase")
 produtoBase.remove()
@@ -15,18 +14,17 @@ function openCloseAddProduto(valor){
 }
 
 function addProduto() {
-    valorProduto = eval(valorProduto.value)
-    valorProduto = valorProduto.toFixed(2)
+    var nomeProduto = document.getElementById("nomeNovoProduto").value
+    var valorProduto = parseFloat(document.getElementById("valorNovoProduto").value)
 
+    valorProduto = valorProduto.toFixed(2)
+    valorProduto = valorProduto.replace(".", ",")
+    
+    console.log(valorProduto)
     const newProduto = produtoBase.cloneNode(true)
     newProduto.querySelector(".tituloProduto").innerHTML = nomeProduto
-    newProduto.querySelector(".Preço").innerHTML = `R$ ${valorProduto.replace(".", ",")}`
-
+    newProduto.querySelector(".Preço").innerHTML = `R$ ${valorProduto}`
     document.getElementById("containerProdutos").append(newProduto)
 
-    var produto = {
-        "produto": newProduto
-    }
 }
-
 
